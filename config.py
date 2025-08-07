140d5203a4927e233293d0cd0b0a2f8fccfbfaa1
jsfuentesjr <116135323+jsfuentesjr@users.noreply.github.com>
2025-08-07 16:06:35 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,19 @@
+# AWS and snowflake configuration
+
+AWS_CONFIG = {
+    "access_key_id": "access_key",
+    "secret_access_key": "secret_access_key",
+    "region": "ap-southeast-1",
+    "s3_bucket": "my-first-project-04",
+    "s3_key": "supply-chain/DataCoSupplyChainDataset.csv"
+}
+
+SNOWFLAKE_CONFIG = {
+    "sfURL": "snowflake_url",
+    "sfUser": "jsfuentesjr",
+    "sfPassword": "password",
+    "sfDatabase": "SUPPLYCHAIN",
+    "sfSchema": "PUBLIC",
+    "sfWarehouse": "FIRST_WAREHOUSE",
+    "sfRole": "ACCOUNTADMIN"
+}
